88a1ff04ce4347e04686f8c04d3a1997de0841eb
Jeremy Borg <jeremy.borg@ixaris.com>
2016-06-17 17:49:47 +0200

    hopefully it works

--- app/run.py
+++ app/run.py
@@ -1,7 +1,19 @@
 import os
 from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+#db variables
+mongoHost = "ds015774.mlab.com"
+mongoPort = 15774
+mongoUser = "root"
+mongoPassword = "PanamaPapers123"
+
+#access collection
+client = MongoClient("mongodb://root:PanamaPapers123@ds015774.mlab.com:15774/heroku_kjcv89lc")
+db = client["heroku_kjcv89lc"]
+collection = db["prediction_logs"]
 
 @app.route("/")
 def hello():
@@ -9,18 +21,8 @@ def hello():
 
 @app.route("/result", methods=["GET"])
 def getResult():
-    return jsonify(
-        {
-            "washCar": False,
-            "predictionBasis": {
-                "daysUntilRain": 3,
-                "precipitation": 0.6
-            },
-            "weatherDetails": {
-                "one": "two"
-            }
-        }
-    )
+    latestUpdate = collection.find().sort({id, -1}).limit(1);
+    return latestUpdate
 
 if __name__ == "__main__":
     app.run(port=5000)
