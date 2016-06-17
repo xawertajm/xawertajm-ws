b630414a6f6bdba13bbc7d3b3441576cd047c1d5
Matthew Farrugia <matthew.farrugia@ixaris.com>
2016-06-17 14:53:23 +0200

    first and last date of rain

--- __init__.py
+++ __init__.py
@@ -7,7 +7,6 @@ __author__ = 'matthew.farrugia'
 #db variables
 mongoHost = "ds015774.mlab.com"
 mongoPort = 15774
-mongoReadPreference = 1
 mongoUser = "root"
 mongoPassword = "PanamaPapers123"
 
@@ -15,19 +14,47 @@ mongoPassword = "PanamaPapers123"
 city = 'Valletta'
 countryCode = 'mt'
 apiKey = '8eab2eb7bab5914e5a9cf5605ac525e2'
-url = 'http://api.openweathermap.org/data/2.5/weather?q=' +city + ',' + countryCode + '&appid=' + apiKey
+
+#urls
+current_weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' +city + ',' + countryCode + '&appid=' + apiKey
 forecast_url = 'http://api.openweathermap.org/data/2.5/forecast?q=' +city + ',' + countryCode + '&appid=' + apiKey
 
-client = MongoClient(
-        "mongodb://root:PanamaPapers123@ds015774.mlab.com:15774/heroku_kjcv89lc"
-        #host=mongoHost,
-        #port=mongoPort
-        #read_preference=mongoReadPreference
-    )
-
+#access collection
+client = MongoClient("mongodb://root:PanamaPapers123@ds015774.mlab.com:15774/heroku_kjcv89lc")
 db = client["heroku_kjcv89lc"]
 collection = db["weather_logs"]
-response = requests.get(forecast_url)
-insertResult = collection.insert_one(json.loads(response.content))
 
-print insertResult;
+#get data from API
+response = requests.get(forecast_url)
+
+#insert data in collection
+#insertResult = collection.insert_one(json.loads(response.content))
+findResult = collection.find()
+
+totalPrecipitation = 0
+isRain = None
+firstDateOfRain = None
+lastDateOfRain = None
+
+#check total precipitation for the next five days
+for document in findResult:
+    for forecast in  document["list"]:
+        try:
+            totalPrecipitation += forecast["rain"]["3h"]
+            if totalPrecipitation > 0 and firstDateOfRain is None:
+                firstDateOfRain = forecast["dt"]
+                isRain = True
+            if forecast["rain"]["3h"] > 0:
+                lastDateOfRain = forecast["dt"]
+        except KeyError:
+            continue
+
+json_str = "{washCar : "",
+  predictionBasis : {
+   daysUntilRain : 3,
+   precipitation : 0.6
+  },"
+
+
+    #for key in document.keys():
+    #    print document[key]
