caffcb848a661e55d0cc6c34e441ee3ac1dc5330
maqqju <matthewcroker@gmail.com>
2016-06-18 12:49:12 +0200

    I don't know if MF tried this already - again (2)

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
-import os
-from flask import Flask, jsonify
+import json
+import time
+import datetime
 from pymongo import MongoClient
+import requests
 
-app = Flask(__name__)
+__author__ = 'matthew.farrugia'
+
+def daysTilRain(firstDayOfRain):
+
+    timeNow = time.time()
+    todayDate = datetime.datetime.fromtimestamp(timeNow)
+    daysToRain = (firstDayOfRain - todayDate).days  # whole days
+    return daysToRain
+
 
 #db variables
 mongoHost = "ds015774.mlab.com"
@@ -15,17 +25,44 @@ client = MongoClient("mongodb://root:PanamaPapers123@ds015774.mlab.com:15774/her
 db = client["heroku_kjcv89lc"]
 collection = db["prediction_logs"]
 
-@app.route("/")
-def hello():
-    return "Hello orld!"
+#api variables
+city = 'Valletta'
+countryCode = 'mt'
+apiKey = '8eab2eb7bab5914e5a9cf5605ac525e2'
 
-@app.route("/result", methods=["GET"])
-def getResult():
-    print("hello")
-    latestUpdate = collection.find().sort({"id", -1}).limit(1)
-    print (latestUpdate.count())
-    print ("ello2")
-    return latestUpdate
+#urls
+current_weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' +city + ',' + countryCode + '&appid=' + apiKey
+forecast_url = 'http://api.openweathermap.org/data/2.5/forecast?q=' +city + ',' + countryCode + '&appid=' + apiKey
 
-if __name__ == "__main__":
-    app.run(port=5001)
+
+while True:
+    #get data from API
+    response = requests.get(forecast_url)
+
+    isRain = None
+    firstDateOfRain = None
+    lastDateOfRain = None
+    totalPrecipitation = 0
+
+    #check total precipitation for the next five days
+    weekForecasts = json.loads(response.content)
+    for forecast in weekForecasts["list"]:
+        try:
+            totalPrecipitation += forecast["rain"]["3h"]
+            if totalPrecipitation > 0 and firstDateOfRain is None:
+                firstDateOfRain = datetime.datetime.fromtimestamp(forecast["dt"])
+                daysUntilRain = daysTilRain(firstDateOfRain)
+                isRain = False if daysUntilRain<3 else True
+        except KeyError:
+            continue
+
+    id = int(time.time()).__str__()
+    prediction = '{"id" : "'+id+'","washCar" : "'+isRain.__str__()+'", "predictionBasis" : { "daysUntilRain" : "'+daysUntilRain.__str__()+'", "precipitation" : "'+totalPrecipitation.__str__()+'ml" }}'
+    prediction_json = json.loads(prediction)
+
+    #insert data in collection
+    collection.insert_one(prediction_json)
+    print "Updated prediction"
+
+    #Update prediction every hour
+    time.sleep(3600)
--- app/app.py
+++ app/app.py
@@ -1,18 +1,8 @@
-import json
-import time
-import datetime
+import os
+from flask import Flask, jsonify
 from pymongo import MongoClient
-import requests
 
-__author__ = 'matthew.farrugia'
-
-def daysTilRain(firstDayOfRain):
-
-    timeNow = time.time()
-    todayDate = datetime.datetime.fromtimestamp(timeNow)
-    daysToRain = (firstDayOfRain - todayDate).days  # whole days
-    return daysToRain
-
+app = Flask(__name__)
 
 #db variables
 mongoHost = "ds015774.mlab.com"
@@ -25,44 +15,17 @@ client = MongoClient("mongodb://root:PanamaPapers123@ds015774.mlab.com:15774/her
 db = client["heroku_kjcv89lc"]
 collection = db["prediction_logs"]
 
-#api variables
-city = 'Valletta'
-countryCode = 'mt'
-apiKey = '8eab2eb7bab5914e5a9cf5605ac525e2'
+@app.route("/")
+def hello():
+    return "Hello orld!"
 
-#urls
-current_weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' +city + ',' + countryCode + '&appid=' + apiKey
-forecast_url = 'http://api.openweathermap.org/data/2.5/forecast?q=' +city + ',' + countryCode + '&appid=' + apiKey
+@app.route("/result", methods=["GET"])
+def getResult():
+    print("hello")
+    latestUpdate = collection.find().sort({"id", -1}).limit(1)
+    print (latestUpdate.count())
+    print ("ello2")
+    return latestUpdate
 
-
-while True:
-    #get data from API
-    response = requests.get(forecast_url)
-
-    isRain = None
-    firstDateOfRain = None
-    lastDateOfRain = None
-    totalPrecipitation = 0
-
-    #check total precipitation for the next five days
-    weekForecasts = json.loads(response.content)
-    for forecast in weekForecasts["list"]:
-        try:
-            totalPrecipitation += forecast["rain"]["3h"]
-            if totalPrecipitation > 0 and firstDateOfRain is None:
-                firstDateOfRain = datetime.datetime.fromtimestamp(forecast["dt"])
-                daysUntilRain = daysTilRain(firstDateOfRain)
-                isRain = False if daysUntilRain<3 else True
-        except KeyError:
-            continue
-
-    id = int(time.time()).__str__()
-    prediction = '{"id" : "'+id+'","washCar" : "'+isRain.__str__()+'", "predictionBasis" : { "daysUntilRain" : "'+daysUntilRain.__str__()+'", "precipitation" : "'+totalPrecipitation.__str__()+'ml" }}'
-    prediction_json = json.loads(prediction)
-
-    #insert data in collection
-    collection.insert_one(prediction_json)
-    print "Updated prediction"
-
-    #Update prediction every hour
-    time.sleep(3600)
+if __name__ == "__main__":
+    app.run(port=5001)
